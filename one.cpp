ed81b6c9cb4c587f9694f7adbf8425531178bd7a
Muzamil Suleman <muzamilworkspace@gmail.com>
2025-02-07 12:36:54 +0500

    i

--- one.cpp
+++ one.cpp
@@ -1,9 +1,35 @@
 #include<iostream>
 using namespace std;
+class BMI{
+    public:
+        double bmi,weight,height;
+        string status;
+    
+    void getInput(){
+
+        cout<<"YOUR WEIGHT(KG): ";
+        cin>>weight;
+        cout<<"YOUR HEIGHT(M): ";
+        cin>>height;
+
+    }
+
+    double calculateBMI(double w ,double h){
+        weight = w/(h*h)*703;
+    }
+    void showStatus(double bmi){
+
+
+
+    }
+
+}
+
 int main(){
-	
-cout<<"HELLO";	
-	
-	
+
+
+
+
+
 return 0;
 }
